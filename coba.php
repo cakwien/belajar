c547fc1da6a580f9d2aec5a606f67d6319ad5ce2
cakwien <58307142+cakwien@users.noreply.github.com>
2019-12-03 08:17:43 +0700

    bener

--- coba.php
+++ coba.php
@@ -6,15 +6,15 @@
         Baris Pertama <br>
         Baris Kedua <br>
         Baris Ketiga <br>
-<<<<<<< HEAD
+
         Baris 4 <br>
         Baris 5 <br>
         Baris 6 <br>
-=======
+
         Baris Keempat <br>
         Baris Kelima <br>
         Baris Keenam<br>
->>>>>>> fungsi_tampil
+        
         
     </body>
 </html>
